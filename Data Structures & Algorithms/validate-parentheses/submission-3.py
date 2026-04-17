f2b30b4dec45b3dba6a4d633dc3591af127ea133
CC_Kuo <90712230+jiachenguoNU@users.noreply.github.com>
2026-04-17 10:29:10 -0500

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = {"(": ")", "{": "}", "[": "]"}
+
+        hashmap0 = defaultdict(str)
+        stack = []
+        if len(s) == 0:
+            return True
+        for i in s:
+            if i in hashmap:
+                stack.append(i)
+            else:
+                if len(stack) != 0 and i == hashmap[stack[-1]]: 
+                    stack.pop()
+                else:
+                    return False 
+        # if len(stack) == 0:
+        #     return True
+        # else:
+        #     return False
+        return True if not stack else False
