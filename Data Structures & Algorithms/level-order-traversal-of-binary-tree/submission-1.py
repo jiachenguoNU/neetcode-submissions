fd282be59fc7efb2b2abcb3e849a9ae4c110c77d
CC_Kuo <90712230+jiachenguoNU@users.noreply.github.com>
2026-05-02 09:29:27 -0500

    Add: level-order-traversal-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-1.py
@@ -0,0 +1,61 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        #这个很基础，就是最简单的level traversal定义
+        #注意这里list是append list，而不是extend
+
+        # if not root:
+        #     return []
+        
+        # res = []
+        # queue = deque()
+        # queue.append(root)
+        # while queue:
+        #     level_list = []
+        #     num_nodes_this_level = len(queue)
+        #     for _ in range(num_nodes_this_level):
+        #         node = queue.popleft()
+        #         level_list.append(node.val)
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #     res.append(level_list)
+        # return res
+
+
+
+
+
+
+
+
+
+
+
+        if not root:
+            return []
+        queue = deque()
+        queue.append(root)
+
+        res = []
+
+        while queue:
+            num_level_nodes = len(queue)
+            res_level = []
+            for _ in range(num_level_nodes):
+                cur = queue.popleft()
+                res_level.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            res.append(res_level)
+        return res
+
